b69ae38f6f37d1597eb0ad87a608ddc8cd0c52b0
Lucía <luciarolon00@gmail.com>
2025-11-01 21:23:42 -0300

    urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from rest_framework.routers import DefaultRouter
 from rest_framework import routers
 from .views import JuegoViewSet, ContactoViewSet
+from django.urls import path
 
 router = DefaultRouter()
 router.register(r"juegos", JuegoViewSet, basename="juego")
