c72390637ae9c0f13b2c5181a0bb466f7378b6b4
Lucía <luciarolon00@gmail.com>
2025-11-03 22:58:13 -0300

    docker

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,11 +3,12 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Juego(models.Model):
     titulo = models.CharField(max_length=100)
-    descripcion = models.TextField()
+    descripcion = models.TextField(blank=True, null=True)
     imagen = models.URLField(max_length=255, blank=True, null=True)
     estrellas = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)]
     )
+    creado_en = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.titulo
