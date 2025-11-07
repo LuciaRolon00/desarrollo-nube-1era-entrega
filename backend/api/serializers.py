9b8c13a035fb8ee2d9a7ef4ce343e00ffc8dd040
Lucía <luciarolon00@gmail.com>
2025-11-06 23:25:04 -0300

    fix #2

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -26,13 +26,13 @@ class ContactoSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'email', 'password')
+        fields = ('id', 'username', 'email', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
-            email=validated_data['email'],
+            email=validated_data.get('email', ''),
             password=validated_data['password']
         )
         return user
